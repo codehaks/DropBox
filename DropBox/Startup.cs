1f09e7bd3ea64390f2cec14d90be94931cb618c4
Hakim Ghods <ghods.hakim@hotmail.com>
2019-01-15 10:43:04 +0330

    Start

--- DropBox/Startup.cs
+++ DropBox/Startup.cs
@@ -11,10 +11,10 @@ namespace DropBox
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -25,10 +25,8 @@ namespace DropBox
                 app.UseDeveloperExceptionPage();
             }
 
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
+            app.UseStaticFiles();
+            app.UseMvc();
         }
     }
 }
