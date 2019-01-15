1862a559bbf26d27fd4c42609fbffbabd1d5b10c
Hakim Ghods <ghods.hakim@hotmail.com>
2019-01-15 10:57:34 +0330

    Static Files

--- DropBox/Controllers/HomeController.cs
+++ DropBox/Controllers/HomeController.cs
@@ -8,6 +8,7 @@ namespace DropBox.Controllers
 {
     public class HomeController : Controller
     {
+        [Route("/")]
         public IActionResult Index()
         {
             return View();
